26a86314b2f5352382f06ec9925eaf90413143a0
Tung Beier <beiertu@mediamarktsaturn.com>
2021-02-11 20:01:11 +0100

    Change font size to 12pt

--- config.def.h
+++ config.def.h
@@ -1,20 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx	= 1;        /* border pixel of windows */
-static const unsigned int snap		= 32;       /* snap pixel */
-static const int showbar			= 1;        /* 0 means no bar */
-static const int topbar				= 1;        /* 0 means bottom bar */
-static const char *fonts[]			= { "JetBrainsMono-Regular:size=10" };
-static const char dmenufont[]		= "JetBrainsMono-Regular:size=10";
-static const char norm_fg[]			= "#88C0D0";
-static const char norm_bg[]			= "#2E3440";
-static const char norm_border[]		= "#4C566A";
-static const char sel_fg[]			= "#EBCB8B";
-static const char sel_bg[]			= "#4C566A";
-static const char sel_border[]		= "#8FBCBB";
-static const char *colors[][3]		= {
-	/*               fg         bg			border   */
+static const unsigned int borderpx = 1;        /* border pixel of windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+static const int showbar           = 1;        /* 0 means no bar */
+static const int topbar            = 1;        /* 0 means bottom bar */
+static const char *fonts[]         = { "JetBrainsMono-Regular:size=12" };
+static const char dmenufont[]      = "JetBrainsMono-Regular:size=12";
+static const char norm_fg[]        = "#88C0D0";
+static const char norm_bg[]        = "#2E3440";
+static const char norm_border[]    = "#4C566A";
+static const char sel_fg[]         = "#EBCB8B";
+static const char sel_bg[]         = "#4C566A";
+static const char sel_border[]     = "#8FBCBB";
+static const char *colors[][3]     = {
+	/*               fg         bg          border   */
 	[SchemeNorm] = { norm_fg,	norm_bg,	norm_border },
 	[SchemeSel]  = { sel_fg,	sel_bg,		sel_border  },
 };
