b2e25af9caa5f329af08c7813583caf9b759a5e1
Tung Beier <beiertu@mediamarktsaturn.com>
2023-04-13 18:50:58 +0200

    feat: apply r-master patch
    See https://dwm.suckless.org/patches/rmaster/

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 /* appearance */
 static const unsigned int borderpx = 1;        /* border pixel of windows */
 static const unsigned int snap     = 32;       /* snap pixel */
+static const int rmaster           = 1;        /* 1 means master-area is initially on the right */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
 static const char *fonts[]         = { FONT":size=9:style=Regular" };
@@ -98,6 +99,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_r,      togglermaster,  {0} },
 	{ MODKEY,                       XK_equal,  view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_equal,  tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_l,      focusmon,       {.i = -1 } },
