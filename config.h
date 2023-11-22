d7a4a26a593f12d549cce5fdd56ae87ff2a47b80
Tung Beier <beiertu@mediamarktsaturn.com>
2023-11-22 09:29:19 +0100

    feat: update font name

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-#define FONT "JetBrainsMono Nerd Font"
+#define FONT "JetBrainsMonoNL NF"
 
 /* appearance */
 static const unsigned int borderpx = 1;        /* border pixel of windows */
